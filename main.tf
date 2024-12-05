ee8333ef5ddb6fc9147047cfb615ce72d0eecad5
al786999 <68891592+al786999@users.noreply.github.com>
2024-12-05 13:19:45 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+# main.tf
+
+provider "aws" {
+  region = "us-east-1" # Change to your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI ID for us-east-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
