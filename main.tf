2195f9b114ed715c2ecff3d1aba58a5045f20221
al786999 <68891592+al786999@users.noreply.github.com>
2024-12-05 14:09:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI ID for us-east-1
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI ID for us-eas888t-1
   instance_type = "t3.micro"
 subnet_id = "subnet-0707d40ddbb9d0818"
 
